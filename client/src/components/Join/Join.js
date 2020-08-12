56a1514fd834d329e501b9adbec8e26edb13bf73
Quang Bui <buiphuquang2412@gmail.com>
2020-08-12 14:05:23 +0300

    Build Join route

--- client/src/components/Join/Join.js
+++ client/src/components/Join/Join.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import cx from 'classnames';
+
+import styles from './Join.module.css';
 
 const Join = () => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
     return (
-        <div>Join</div>
+        <div className={styles.JoinOuterContainer}>
+            <div className={styles.joinInnerContainer}>
+                <h1 className={styles.heading}>Join</h1>
+                <div><input placeholder="Name" className={styles.joinInput} type="text" onChange={e => setName(e.target.value)}/></div>
+                <div><input placeholder="Room" className={styles.joinInput} type="text" onChange={e => setRoom(e.target.value)}/></div>
+                <Link onClick={e => (!name || !room) ? e.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+                    <button className={cx(styles.button, styles.mt20)} type="button">Join</button>
+                </Link>
+            </div>
+        </div>
     );
 };
 
