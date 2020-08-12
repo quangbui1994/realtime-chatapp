4b012196f2cb837175e5f39e5863a2de2529761d
Quang Bui <buiphuquang2412@gmail.com>
2020-08-12 14:22:12 +0300

    Setup socket.io on the client side

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import queryString from 'query-string';
 
-const Chat = () => {
+let socket;
+
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT = 'localhost:5000';
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);
+        setName(name);
+        setRoom(room);
+        console.log(name, room);
+    }, [ENDPOINT, location.search]);
+
     return (
         <div>Chat</div>
     );
